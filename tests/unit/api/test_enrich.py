448920bad3d86caa934f4efb286e4b9cb1d6698f
MaxMhonc <71133140+MaxMhonc@users.noreply.github.com>
2020-12-31 10:23:13 +0200

    removed type validation (#18)

--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 
 from pytest import fixture
 
-from api.schemas import OBSERVABLE_TYPE_CHOICES
 from .utils import get_headers
 
 
@@ -22,11 +21,6 @@ def invalid_json_value():
     return [{'type': 'ip', 'value': ''}]
 
 
-@fixture(scope='module')
-def invalid_json_type():
-    return [{'type': 'strange', 'value': 'cisco.com'}]
-
-
 def test_enrich_call_with_valid_jwt_but_invalid_json_value(
         route, client, valid_jwt, invalid_json_value,
         invalid_json_expected_payload
@@ -37,20 +31,6 @@ def test_enrich_call_with_valid_jwt_but_invalid_json_value(
     assert response.status_code == HTTPStatus.OK
     assert response.json == invalid_json_expected_payload(
         "{0: {'value': ['Field may not be blank.']}}"
-    )
-
-
-def test_enrich_call_with_valid_jwt_but_invalid_json_type(
-        route, client, valid_jwt, invalid_json_type,
-        invalid_json_expected_payload
-):
-    allowed_fields = ", ".join(map(repr, OBSERVABLE_TYPE_CHOICES))
-    response = client.post(route,
-                           headers=get_headers(valid_jwt),
-                           json=invalid_json_type)
-    assert response.status_code == HTTPStatus.OK
-    assert response.json == invalid_json_expected_payload(
-        '{0: {\'type\': ["Must be one of: ' + allowed_fields + '."]}}'
     )
 
 
